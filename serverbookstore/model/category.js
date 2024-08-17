9bf5c0c0a018d4fad82ec863fd26307ec1636a1e
phatvo2201 <49409529+phatvo2201@users.noreply.github.com>
2024-08-17 19:34:18 +0700

    Delete serverbookstore directory

--- serverbookstore/model/category.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require('mongoose')
-
-const categorySchema= new mongoose.Schema({
-
-    name:String,
-    Book_id:[{type:mongoose.Types.ObjectId}]
-})
-module.exports=mongoose.model("category",categorySchema)
-
